dbd9556d9326f479c8db5690f7e6f69889a44553
mamdou1 <mamadoudiouma826@gmail.com>
2026-03-21 18:52:18 +0000

    le départ

--- models/index.js
+++ models/index.js
@@ -1,18 +1,19 @@
-const { Sequelize, DataTypes } = require("sequelize"); 
+const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
 const db = {};
 
 db.Contact = require("./Contact.models")(sequelize, DataTypes);
 db.Enterprise = require("./Entreprise.models")(sequelize, DataTypes);
-db.Notifications = require("./Notifications.models")(sequelize, DataTypes); 
+db.Notifications = require("./Notifications.models")(sequelize, DataTypes);
 db.Service = require("./Service.models")(sequelize, DataTypes);
 db.Solution = require("./Solution.models")(sequelize, DataTypes);
 db.ServiceSolution = require("./ServiceSolution.models")(sequelize, DataTypes);
-db.Temoignage = require("./Temoinage.models")(sequelize, DataTypes); 
+db.Temoignage = require("./Temoinage.models")(sequelize, DataTypes);
 db.Users = require("./Users.models")(sequelize, DataTypes);
 db.Video = require("./Video.models")(sequelize, DataTypes);
 db.Token = require("./token.model")(sequelize, DataTypes);
+db.HistoriqueLog = require("./HistoriqueLog.model")(sequelize, DataTypes);
 
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
